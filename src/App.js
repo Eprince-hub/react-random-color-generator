2ae073387e27a0d764964c42d8ea73e441d96f2e
VICTOR EJIKE NWOSU <74430629+Eprince-hub@users.noreply.github.com>
2021-09-14 22:07:16 +0200

    update app.js file

--- src/App.js
+++ src/App.js
@@ -2,29 +2,6 @@ import './App.css';
 import LeftColorBox from './Components/LeftColorBox';
 import RightColorBox from './Components/RightColorBox';
 
-// import logo from './logo.svg';
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
-
 function App() {
   // state here
   return (
